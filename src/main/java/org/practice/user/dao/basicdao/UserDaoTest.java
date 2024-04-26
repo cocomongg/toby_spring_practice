ee8cb93df265fdbeb13b1c1bde32425d265cc421
cocomongg <pjm4142@naver.com>
2024-04-26 20:40:42 +0900

    Junit5 적용 및 테스트 코드 추가

--- src/main/java/org/practice/user/dao/basicdao/UserDaoTest.java
+++ src/main/java/org/practice/user/dao/basicdao/UserDaoTest.java
@@ -1,15 +1,19 @@
 package org.practice.user.dao.basicdao;
 
+import org.junit.jupiter.api.Test;
 import org.practice.user.dao.User;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.GenericXmlApplicationContext;
 
 import java.sql.SQLException;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 public class UserDaoTest {
-    public static void main(String[] args) throws SQLException, ClassNotFoundException {
-//        ApplicationContext context = new AnnotationConfigApplicationContext(DaoFactory.class);
-        ApplicationContext context = new GenericXmlApplicationContext("applicatFionContext.xml");
+    
+    @Test
+    public void addAndGet() throws SQLException {
+        ApplicationContext context = new GenericXmlApplicationContext("applicationContext.xml");
         UserDao userDao = context.getBean("userDao", UserDao.class);
 
         User user = new User();
@@ -18,10 +22,9 @@ public class UserDaoTest {
         user.setPassword("firstUserPassword");
 
         userDao.addUser(user);
-        System.out.println("user 등록 성공");
+        User getUser = userDao.getById(user.getId());
 
-        User findUser = userDao.getById(user.getId());
-        System.out.printf("user 조회 성공 id: %s, name: %s, password: %s%n",
-                findUser.getId(), findUser.getName(), findUser.getPassword());
+        assertThat(getUser.getName().equals(user.getName()));
+        assertThat(getUser.getId().equals(user.getId()));
     }
 }
