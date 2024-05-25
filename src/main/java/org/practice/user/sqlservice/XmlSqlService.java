607f2c0e284c63fc1c9eff3931452bbdbf27cc20
cocomongg <pjm4142@naver.com>
2024-05-26 00:03:19 +0900

    refactor XmlSqlService

--- src/main/java/org/practice/user/sqlservice/XmlSqlService.java
+++ src/main/java/org/practice/user/sqlservice/XmlSqlService.java
@@ -2,8 +2,10 @@ package org.practice.user.sqlservice;
 
 import org.practice.user.sqlservice.jaxb.SqlType;
 import org.practice.user.sqlservice.jaxb.Sqlmap;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.util.StringUtils;
 
+import javax.annotation.PostConstruct;
 import javax.xml.bind.JAXBContext;
 import javax.xml.bind.JAXBException;
 import javax.xml.bind.Unmarshaller;
@@ -14,13 +16,20 @@ import java.util.Map;
 public class XmlSqlService implements SqlService{
     private Map<String, String> sqlMap = new HashMap<>();
 
-    public XmlSqlService() {
+    private String sqlmapFile;
+
+    public void setSqlmapFile(String sqlmapFile) {
+        this.sqlmapFile = sqlmapFile;
+    }
+
+    @PostConstruct
+    public void loadSql() {
         String contextPath = Sqlmap.class.getPackage().getName();
 
         try {
             JAXBContext context = JAXBContext.newInstance(contextPath);
             Unmarshaller unmarshaller = context.createUnmarshaller();
-            Sqlmap sqlmap = (Sqlmap) unmarshaller.unmarshal(getXmlFile("sqlmap.xml"));
+            Sqlmap sqlmap = (Sqlmap) unmarshaller.unmarshal(getXmlFile(this.sqlmapFile));
 
             for(SqlType sql : sqlmap.getSql()) {
                 sqlMap.put(sql.getKey(), sql.getValue());
